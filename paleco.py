f43d148df55e9ea83be0d650ef82521b15c6c82a
james <useruser3.pollock@gmail.com>
2018-07-18 03:02:33 +0100

    added 1st cog

---
+++ paleco.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+sesh = ""
+class Mysession:
+    """My custom cog for A.S.S"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def session(self,ctx):
+        """get context"""
+        server = ctx.message.server
+        await self.bot.say(server.get_channel('434434257348198400').topic)
+
+
+def setup(bot):
+    bot.add_cog(Mysession(bot))
