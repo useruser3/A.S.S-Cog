d275fbe939b0330f2d459e3ffe770b54bead721b
james pollock <useruser3.pollock@gmail.com>
2018-09-04 17:43:36 +0100

    awaiting values
    test

--- session/session.py
+++ session/session.py
@@ -80,15 +80,15 @@ class Session:
     async def slist(self,ctx):
         """lists all sessions"""
         embed=discord.Embed(title='Session List')
-        if self.config.sessions.apex() != "none":
+        if await self.config.sessions.apex() != "none":
             embed.add_field(name='APEX: ', value=await self.config.sessions.apex(), inline=False)
-        if self.config.sessions.apex2() != "none":   
+        if await self.config.sessions.apex2() != "none":   
             embed.add_field(name='APEX2: ', value=await self.config.sessions.apex2(), inline=False)
-        if self.config.sessions.apex3() != "none":
+        if await self.config.sessions.apex3() != "none":
             embed.add_field(name='APEX3: ', value=await self.config.sessions.apex3(), inline=False)
-        if self.config.sessions.apex4() != "none":
+        if await self.config.sessions.apex4() != "none":
             embed.add_field(name='APEX4: ', value=await self.config.sessions.apex4(), inline=False)
-        if self.config.sessions.apex5() != "none":
+        if await self.config.sessions.apex5() != "none":
             embed.add_field(name='APEX5: ', value=await self.config.sessions.apex5(), inline=False)
         if ctx.channel.id == acechannel:
                 embed.add_field(name='ACE: ', value=await self.config.sessions.ace(), inline=False)
