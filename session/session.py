1b38697c33caca7e3a0198dee6c548d2e8453a4f
james pollock <useruser3.pollock@gmail.com>
2018-09-17 01:58:57 +0100

    added name function

--- session/session.py
+++ session/session.py
@@ -1,4 +1,4 @@
-from redbot.core import commands
+from redbot.core import checks,commands
 from redbot.core import Config
 import discord
 acechannel = 474639017422356491
@@ -21,16 +21,39 @@ class Session:
         self.config = Config.get_conf(self, identifier=1234567890)
         default_global = {
             "sessions": {
-                "apex": "There is no session set. Please set one using session set followed by the session ID",
-                "apex2": "There is no session set. Please set one using session set followed by the session ID",
-                "apex3": "There is no session set. Please set one using session set followed by the session ID",
-                "apex4": "There is no session set. Please set one using session set followed by the session ID",
-                "apex5": "There is no session set. Please set one using session set followed by the session ID",
-                "ace": "There is no session set. Please set one using session set followed by the session ID"
-
-        
+                "session1":{
+                    "defaultname": "session1",
+                    "name": "false:",
+                    "id": "none"
+                },
+                "session2":{
+                    "defaultname": "session2",
+                    "name": "EVENT",
+                    "id": "none"
+                },
+                "session3":{
+                    "defaultname": "session3",
+                    "name": "none",
+                    "id": "none"
+                },
+                "session4":{
+                    "defaultname": "session4",
+                    "name": False,
+                    "id": "none"
+                },
+                "session5":{
+                    "defaultname": "session5",
+                    "name": False,
+                    "id": "none"
+                },
+                "session6":{
+                    "defaultname": "session6",
+                    "name": False,
+                    "id": "noneeeee"
+                }
             }
         }
+
         default_user = {
             "info": {
                 "name": "none",
@@ -48,6 +71,7 @@ class Session:
         #end config
 
     #session commands
+
     @is_channel_not(455080960455868417)
     @commands.group(autohelp=False)
     async def session(self,ctx):        
@@ -55,75 +79,67 @@ class Session:
         if ctx.invoked_subcommand is None:
             """lists all active sessions"""
             embed=discord.Embed(title='Session List')
-            if await self.config.sessions.apex() != "none":
-                embed.add_field(name='APEX: ', value=await self.config.sessions.apex(), inline=False)
-            if await self.config.sessions.apex2() != "none":   
-                embed.add_field(name='APEX2: ', value=await self.config.sessions.apex2(), inline=False)
-            if await self.config.sessions.apex3() != "none":
-                embed.add_field(name='APEX3: ', value=await self.config.sessions.apex3(), inline=False)
-            if await self.config.sessions.apex4() != "none":
-                embed.add_field(name='APEX4: ', value=await self.config.sessions.apex4(), inline=False)
-            if await self.config.sessions.apex5() != "none":
-                embed.add_field(name='APEX5: ', value=await self.config.sessions.apex5(), inline=False)
-            if ctx.channel.id == acechannel and await self.config.sessions.ace() != "none":
-                embed.add_field(name='ACE: ', value=await self.config.sessions.ace(), inline=False)
+            if await self.config.sessions.session1.id() != "none":
+                embed.add_field(name=await self.config.sessions.session1.name(), value=await self.config.sessions.session1.id(), inline=False)
+            if await self.config.sessions.session2.id() != "none":
+                embed.add_field(name=await self.config.sessions.session2.name(), value=await self.config.sessions.session2.id(), inline=False)
+            if await self.config.sessions.session3.id() != "none":
+               embed.add_field(name=await self.config.sessions.session3.name(), value=await self.config.sessions.session3.id(), inline=False)
+            if await self.config.sessions.session4.id() != "none":
+                embed.add_field(name=await self.config.sessions.session4.name(), value=await self.config.sessions.session4.id(), inline=False)
+            if await self.config.sessions.session5.id() != "none":
+               embed.add_field(name=await self.config.sessions.session5.name(), value=await self.config.sessions.session5.id(), inline=False)
+            if ctx.channel.id == acechannel and await self.config.sessions.session6.id() != "none":
+                embed.add_field(name=await self.config.sessions.session6.name(), value=await self.config.sessions.session6.id(), inline=False)
             await ctx.send(embed=embed)
 
     @session.command(name="set")
     async def set_session(self,ctx,stype,*,text):
         """Type set and the team you want to update followed by the new session ID (stype is the name of the team colour and text is the session ID)""" 
-        if stype == "apex":
-            await ctx.send("```" + "The session ID for " + stype + " is now:" + " " + text + "```")
-            await self.config.sessions.apex.set(text)
-        elif stype == "apex2":
-            await ctx.send("```" + "The session ID for " + stype + " is now:" + " " + text + "```")
-            await self.config.sessions.apex2.set(text)
-        elif stype == "apex3":
-            await ctx.send("```" + "The session ID for " + stype + " is now:" + " " + text + "```")
-            await self.config.sessions.apex3.set(text)
-        elif stype == "apex4":
-            await ctx.send("```" + "The session ID for " + stype + " is now:" + " " + text + "```")
-            await self.config.sessions.apex4.set(text)
-        elif stype == "apex5":
-            await ctx.send("```" + "The session ID for " + stype + " is now:" + " " + text + "```")
-            await self.config.sessions.apex5.set(text)
-        elif stype == "ace" and ctx.channel.id == acechannel:
-            await ctx.send("```" + "The session ID for " + stype + " is now:" + " " + text + "```")
-            await self.config.sessions.ace.set(text)
+        newtext = text.replace(" ", "_")
+        if stype == await self.config.sessions.session1.name() or stype == await self.config.sessions.session1.defaultname():
+            await ctx.send("```" + "The session ID for " + stype + " is now:" + " " + newtext + "```")
+            await self.config.sessions.session1.id.set(newtext)
+        elif stype == await self.config.sessions.session2.name() or stype == await self.config.sessions.session2.defaultname():
+            await ctx.send("```" + "The session ID for " + stype + " is now:" + " " + newtext + "```")
+            await self.config.sessions.session2.id.set(newtext)
+        elif stype == await self.config.sessions.session3.name() or stype == await self.config.sessions.session3.defaultname():
+            await ctx.send("```" + "The session ID for " + stype + " is now:" + " " + newtext + "```")
+            await self.config.sessions.session3.id.set(newtext)
+        elif stype == await self.config.sessions.session4.name() or stype == await self.config.sessions.session4.defaultname():
+            await ctx.send("```" + "The session ID for " + stype + " is now:" + " " + newtext + "```")
+            await self.config.sessions.session4.id.set(newtext)
+        elif stype == await self.config.sessions.session5.name() or stype == await self.config.sessions.session5.defaultname():
+            await ctx.send("```" + "The session ID for " + stype + " is now:" + " " + newtext + "```")
+            await self.config.sessions.session5.id.set(newtext)
+        elif stype == await self.config.sessions.session6.name() or stype == await self.config.sessions.session6.defaultname() and ctx.channel.id == acechannel:
+            await ctx.send("```" + "The session ID for " + stype + " is now:" + " " + newtext + "```")
+            await self.config.sessions.session6.id.set(newtext)
         else:
             await ctx.send("invalid team")
-       
 
-    @session.command(name="main")
-    async def sapx(self,ctx):
-        """tells you the session for apex"""
-        await ctx.send("The current session for apex is " + "```" + await self.config.sessions.apex() + "```")
-
-    @session.command(name="apex2")
-    async def sapx2(self,ctx):
-        """tells you the session for apex2"""
-        await ctx.send("The current session for apex2 is " + "```" + await self.config.sessions.apex2() + "```")
-
-    @session.command(name="apex3")
-    async def sapx3(self,ctx):
-        """tells you the session for apex3"""
-        await ctx.send("The current session for apex3 is " + "```" + await self.config.sessions.apex3() + "```")
-
-    @session.command(name="apex4")
-    async def sapx4(self,ctx):
-        """tells you the session for apex4"""
-        await ctx.send("The current session for apex4 is " + "```" + await self.config.sessions.apex4() + "```")
-
-    @session.command(name="apex5")
-    async def sapx5(self,ctx):
-        """tells you the session for apex5"""
-        await ctx.send("The current session for apex5 is " + "```" + await self.config.sessions.apex5() + "```")
-
-    @session.command(name="ace")
-    async def sace(self,ctx):
-        """tells you the session for ace"""
-
-        if ctx.channel.id == acechannel:
-            await ctx.send("The current session for ace is " + "```" + await self.config.sessions.ace() + "```")
+    @session.command(name="setname")
+    @checks.mod_or_permissions()
+    async def set_session_name(self,ctx,*,str):
+        """some descriptive text will be here soon-james"""
+        stype,text = str.split(",")
+        if stype == await self.config.sessions.session1.name() or stype == await self.config.sessions.session1.defaultname():
+            await ctx.send("```" + "The session Name for " + stype + " is now:" + " " + text + "```")
+            await self.config.sessions.session1.name.set(text)
+        elif stype == await self.config.sessions.session2.name() or stype == await self.config.sessions.session2.defaultname():
+            await ctx.send("```" + "The session Name for " + stype + " is now:" + " " + text + "```")
+            await self.config.sessions.session2.name.set(text)
+        elif stype == await self.config.sessions.session3.name() or stype == await self.config.sessions.session3.defaultname():
+            await ctx.send("```" + "The session Name for " + stype + " is now:" + " " + text + "```")
+            await self.config.sessions.session3.name.set(text)
+        elif stype == await self.config.sessions.session4.name() or stype == await self.config.sessions.session4.defaultname():
+            await ctx.send("```" + "The session Name for " + stype + " is now:" + " " + text + "```")
+            await self.config.sessions.session4.name.set(text)
+        elif stype == await self.config.sessions.session5.name() or stype == await self.config.sessions.session5.defaultname():
+            await ctx.send("```" + "The session Name for " + stype + " is now:" + " " + text + "```")
+            await self.config.sessions.session5.name.set(text)
+        elif stype == await self.config.sessions.session6.name() or stype == await self.config.sessions.session6.defaultname() and ctx.channel.id == acechannel:
+            await ctx.send("```" + "The session Name for " + stype + " is now:" + " " + text + "```")
+            await self.config.sessions.session6.name.set(text)
         else:
-            return
+            await ctx.send("invalid team")
