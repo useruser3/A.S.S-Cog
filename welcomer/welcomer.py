371d455f14f63c61b75e7dc6fc4f234b67c3b274
useruser3 <useruser3.pollock@gmail.com>
2018-10-12 16:14:11 +0100

    Update welcomer/welcomer.py

--- welcomer/welcomer.py
+++ welcomer/welcomer.py
@@ -1,6 +1,8 @@
 from redbot.core import commands
 from redbot.core import Config
 import discord
+import asyncio
+
 acechannel = 474639017422356491
 
 def is_channel(channel_id):
@@ -28,14 +30,47 @@ class Welcomer(BaseCog):
             self._config = ctx.bot.get_cog("Session").config
         return self._config
 
-    async  def on_member_join(self, member):
-        server = member.guild.name
+    async def on_member_join(self, member):
+        server = member.guild
         channel = self.bot.get_channel(481390232521015296)
-        await channel.send(f"Welcome {member.mention} to {server}. Please read pinned message and head over to <#481295578530185226> for help getting your roles and access to channels. Thank you enjoy.")
+        role = discord.utils.get(server.roles, id=455061046390947840)
+        await channel.send(f"Welcome {member.mention} to {server.name}. Please read pinned message and head over to <#481295578530185226> for help getting your roles and access to channels. Thank you enjoy.")
+        await member.add_roles(role)
+        channel = self.bot.get_channel(481295578530185226)
+        asyncio.sleep(10)
+        await channel.send(f"{member.mention} please tell me what you are here for? say 1 for monster hunter world \n or 2 for destiny")
+        def mhwordestiny(m):
+            return m.content == "1" and  m.channel == channel or m.content == "2" and  m.channel == channel
+        msg = await self.bot.wait_for('message', check=mhwordestiny)
+        if m.content.startswith("1"):
+            await channel.send(f"{member} you are here for monster hunter world")
+        elif m.content.startswith("2"):
+            await channel.send(f"{member} you are here for destiny")
+
+
+    # async def on_message(self,message):
+    #     channel = self.bot.get_channel(478891177232564225)
+    #     member = message.author
+    #     def mhwordestiny(m):
+    #         return m.content == "1" and  m.channel == channel or m.content == "2" and  m.channel == channel
+    #     msg = await self.bot.wait_for('message', check=mhwordestiny)
+    #     if message.content.startswith("1"):
+    #         await channel.send(f"{member} you are here for monster hunter world")
+    #     elif m.content.startswith("2"):
+    #         await channel.send(f"{member} you are here for monster hunter world")
 
     async  def on_member_remove(self, member):
         channel = self.bot.get_channel(481390232521015296)
         await channel.send(f"Sorry to see you go {member}")
+
+    # async def on_member_update(self, before,member):
+    #         channel = self.bot.get_channel(478891177232564225)
+    #         server=member.guild
+    #         role = discord.utils.get(server.roles, id=455061046390947840)
+    #         if member.id == 141908685738737664:
+    #             await channel.send(role)
+    #             await member.add_roles(role)
+    #             await channel.send("role added")
     
 #welcomer commands
     @commands.group(autohelp=False)
@@ -44,7 +79,7 @@ class Welcomer(BaseCog):
         self.get_config(ctx)
         user_data = self._config.user(ctx.author)
         if ctx.invoked_subcommand is None:
-            await ctx.send("boop")
+            return
 
     @commands.command(hidden=True)
     async def meow(self,ctx,channel:int,*,text):
